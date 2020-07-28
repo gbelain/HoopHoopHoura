0c6c2e2a99e461227cb2f0c9d3f17f574cd17169
gbelain <45588824+Epsar@users.noreply.github.com>
2020-07-28 11:46:38 +0200

    hoop center found

--- ball-tracking/ball_tracking.py
+++ ball-tracking/ball_tracking.py
@@ -16,48 +16,55 @@ import time
 ######################################################################################
 
 
-def findHoopLocation(vs):
+def findHoopCenterLocation(vs):
     vs.set(1, 5)
     boleen, frame = vs.read()
-
     frame = imutils.resize(frame, width=600)
-    cv2.imwrite("la frame de base"+'.jpg', frame)
-
+    # cv2.imwrite("la frame de base"+'.jpg', frame)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    cv2.imwrite("la frame blurred"+'.jpg', blurred)
-
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imwrite("la frame hsv"+'.jpg', hsv)
-
+    # cv2.imwrite("la frame blurred"+'.jpg', blurred)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    # cv2.imwrite("la frame hsv"+'.jpg', hsv)
     mask = cv2.inRange(hsv, redLower, redUpper)
-    cv2.imwrite("le mask"+'.jpg', mask)
-
+    # cv2.imwrite("le mask"+'.jpg', mask)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
-    cv2.imwrite("le mask apres traitement"+'.jpg', mask)
-
-    # cnts = cv2.findContours(
-    #     mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # cnts = imutils.grab_contours(cnts)
-    # center = None
-
+    # blurredMask = cv2.GaussianBlur(mask, (11, 11), 0)
+    # cv2.imwrite("le mask apres traitement"+'.jpg', mask)
+    cnts = cv2.findContours(
+        mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
     # only proceed if at least one contour was found
-    # if len(cnts) > 0:
-    #     # find the largest contour in the mask, then use
-    #     # it to compute the minimum enclosing circle and
-    #     # centroid
-    #     c = max(cnts, key=cv2.contourArea)
-    #     ((x, y), radius) = cv2.minEnclosingCircle(c)
-    #     M = cv2.moments(c)
-    #     center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    if len(cnts) > 0:
+        # countShapesSides = 10
+        circleContour = None
+        # for c in cnts:
+        #     # compute the center of the contour
+        #     # M = cv2.moments(c)
+        #     # cX = int(M["m10"] / M["m00"])
+        #     # cY = int(M["m01"] / M["m00"])
 
-    #     # only proceed if the radius meets a minimum size
-    #     if radius > 10:
-    #         # draw the circle and centroid on the frame,
-    #         # then update the list of tracked points
-    #         cv2.circle(frame, (int(x), int(y)), int(radius),
-    #                    (0, 255, 255), 2)
-    #         cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        #     peri = cv2.arcLength(c, True)
+        #     approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+        #     print len(approx)
+        #     if len(approx) < countShapesSides:
+        #         countShapesSides = len(approx)
+        #         circleContour = c
+        circleContour = max(cnts, key=cv2.contourArea)
+        # draw the contour and center of the shape on the image
+
+        M = cv2.moments(circleContour)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.drawContours(frame, [circleContour], -1, (0, 255, 0), 2)
+        cv2.circle(frame, (cX+16, cY-14), 3, (255, 255, 255), -1)
+        # show the image
+        cv2.imshow("les contours", frame)
+        cv2.waitKey(0)
+        return (cX+16, cY-14)
+    else:
+        print "pas de contour trouve"
+        return None
 
 
 def findAndShowBallLocation(frame, orangeLower, orangeUpper):
@@ -127,22 +134,21 @@ def findAndShowBallLocation(frame, orangeLower, orangeUpper):
 orangeLower = (7, 100, 20)
 orangeUpper = (22, 250, 255)
 
-redLower = (0, 150, 20)
-redUpper = (5, 255, 255)
+redLower = (0, 100, 20)
+redUpper = (10, 255, 255)
 
 dequeLength = 64
 videoName = "test_video.MOV"
 
 pts = deque(maxlen=dequeLength)
 
-vs2 = cv2.VideoCapture(videoName)
+vsHoop = cv2.VideoCapture(videoName)
+
 time.sleep(2.0)
 
-findHoopLocation(vs2)
+(xHoop, yHoop) = findHoopCenterLocation(vsHoop)
+vsHoop.release()
 
-vs2.release()
-
-# grab a reference to the video file
 vs = cv2.VideoCapture(videoName)
 
 # allow the camera or video file to warm up
