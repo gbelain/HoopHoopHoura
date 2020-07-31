62e451fb3071845e9af312afdccd7234964e7ebd
gbelain <45588824+Epsar@users.noreply.github.com>
2020-07-31 11:42:32 +0200

    factorisation DisplayTrackingOnFrame

--- ball-tracking/ball_tracking.py
+++ ball-tracking/ball_tracking.py
@@ -141,6 +141,34 @@ def CheckTrajectory(whileCount, ballCenter, pts):
     return ballCenter
 
 
+def DisplayTrackingOnFrame(pts, shotTaken, thisShotWasMade, hoopLocation, frame):
+    # loop over the set of tracked points
+    for i in range(1, len(pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        thickness = int(np.sqrt(dequeLength / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    if shotTaken:
+        cv2.putText(frame, " en train de tirer", (425, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.6, (255, 255, 255), 2)
+    # show the frame to our screen
+    cv2.line(frame, (hoopLocation[0], hoopLocation[1]-13),
+             (hoopLocation[0], hoopLocation[1]+13), (255, 0, 0), 5)
+    # cv2.line(frame, (hoopLocation[0]-75, 1),
+    #          (hoopLocation[0]-75, 500), (0, 0, 255), 2)
+    # cv2.line(frame, (hoopLocation[0]+60, 1),
+    #  (hoopLocation[0]+60, 500), (0, 0, 255), 2)
+    if thisShotWasMade:
+        cv2.putText(frame, "PANIER MARQUE !!", (420, 70), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.6, (0, 0, 255), 2)
+    return frame
+
+
 ######################################################################################
 # define the lower and upper boundaries of the "orange"
 # ball in the HSV color space, then initialize the
@@ -184,41 +212,16 @@ while True:
         IsTakingAShot(ballCenter[0])
         ShotMade(ballCenter, hoopLocation)
 
-    # loop over the set of tracked points
-    for i in range(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        thickness = int(np.sqrt(dequeLength / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+    frame = DisplayTrackingOnFrame(
+        pts, shotTaken, thisShotWasMade, hoopLocation, frame)
 
-    if shotTaken:
-        cv2.putText(frame, " en train de tirer", (425, 50), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6, (255, 255, 255), 2)
-    # show the frame to our screen
-    cv2.line(frame, (hoopLocation[0], hoopLocation[1]-13),
-             (hoopLocation[0], hoopLocation[1]+13), (255, 0, 0), 5)
-    # cv2.line(frame, (hoopLocation[0]-75, 1),
-    #          (hoopLocation[0]-75, 500), (0, 0, 255), 2)
-    # cv2.line(frame, (hoopLocation[0]+60, 1),
-    #  (hoopLocation[0]+60, 500), (0, 0, 255), 2)
-    if thisShotWasMade:
-        cv2.putText(frame, "PANIER MARQUE !!", (420, 70), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6, (0, 0, 255), 2)
     cv2.imshow("Frame", frame)
-
     out.write(frame)
-
-    key = cv2.waitKey(1) & 0xFF
-
+    whileCount += 1
     # if the 'q' key is pressed, stop the loop
+    key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
-
-    whileCount += 1
 
 out.release()
 # release the video/camera
